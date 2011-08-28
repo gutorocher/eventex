5a1b26629233ea914a2065219a73e9b39ec8e1e9
Gustavo Rocha <gustavo.rochanetto@gmail.com>
2011-08-28 03:24:52 -0300

    update subscription

--- subscription/admin.py
+++ subscription/admin.py
@@ -8,6 +8,8 @@ Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 """
 from django import admin
 from subscription.models import Subscription
-from django.contrib import admin
 
-admin.site.register(Subscription)
+class Subscription(admin.ModelAdmin):
+	list_display = ('nome','email','phone','created_at')
+
+admin.site.register(Subscription, SubscriptionAdmin)
