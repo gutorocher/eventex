f2127ab549d7f251d58d301e7731b99efce7f232
Gustavo Rocha <gustavo.rochanetto@gmail.com>
2011-09-02 15:08:51 -0300

    modulo admin version01

--- subscription/admin.py
+++ subscription/admin.py
@@ -9,9 +9,12 @@ class SubscriptionAdmin(admin.ModelAdmin):
 	list_display = ('name','cpf','email','phone','created_at', 
 	'subscribed_today')
 
+	search_fields = ('name', 'cpf','email','phone','created_at')
+
+
 	def subscribed_today(self, obj):
 		return obj.created_at.date() == datetime.date.today()
-	subscribed_today.short_description = 'Inscrito hoje?'
-
+	subscribed_today.short_description = 'Inscrito hoje ?'
+	subscribed_today.boolean = True
 
 admin.site.register(Subscription, SubscriptionAdmin)
