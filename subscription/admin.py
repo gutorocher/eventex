fd482fbb49c7f163229f605396a2601cf645e21d
Gustavo Rocha <gustavo.rochanetto@gmail.com>
2011-09-04 14:38:39 -0300

    modulo admin version02

--- subscription/admin.py
+++ subscription/admin.py
@@ -6,15 +6,20 @@ from subscription.models import Subscription
 from django.utils.translation import ugettext as _
 from django.utils.translation import ungettext
 
-
 class SubscriptionAdmin(admin.ModelAdmin):
-	
-	list_display  = ('name', 'email', 'phone', 'created_at', 'subscription_today', 'paid')		
+
+	list_display  = ('name', 'email', 'phone', 'created_at','subscribed_today','paid')		
 	date_hierarchy = 'created_at' 	
 	search_fields = ('name', 'cpf', 'email', 'phone', 'created_at') 	
 	list_filter = ('paid', )
 
+	def subscribed_today(self,obj):
+		return obj.created_at.date() == datetime.date.today()
+	subscribed_today.short_description = 'Inscrito hoje ?'
+	subscribed_today.boolean = True
+
 	actions = ['mark_as_paid']
+
 
 	def mark_as_paid (self, request, queryset):
 		count = queryset.update(paid=True)
@@ -28,9 +33,6 @@ class SubscriptionAdmin(admin.ModelAdmin):
 		self.message_user(request, msg)
 	mark_as_paid.short_description = _(u"Marcar como pagas")
 
-	def subscribed_today(self, obj):
-		return obj.created_at.date() == datetime.date.today()
-	subscribed_today.short_description = 'Inscrito hoje ?'
-	subscribed_today.boolean = True
+
 
 admin.site.register(Subscription, SubscriptionAdmin)
