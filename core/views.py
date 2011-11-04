da61fd3fe5f90157c33131ef19fd80e341e79247
Gustavo Rocha <gustavo.rochanetto@gmail.com>
2011-11-04 15:42:42 -0200

    alteracao da estrutura

--- core/views.py
+++ core/views.py
@@ -3,10 +3,12 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from djanfo.conf import settings
 
 def homepage(request):
-	context = RequestContext(request)
+
+	context = { 'MEDIA_URL': settings.MEDIA_URL}
+	
 	return render_to_response('index.html', context)
 
 
